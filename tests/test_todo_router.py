b2219f8e656a1a8202355a89e3990013f4d3b046
Roman2543 <ghostzrome2543@gmail.com>
2022-03-18 11:30:06 +0700

    update+delete

--- tests/test_todo_router.py
+++ tests/test_todo_router.py
@@ -20,3 +20,26 @@ def test_post_todo_insert(db):
     assert response.json()[0]["description"] == "string"
     assert response.json()[0]["completed"] == True
     assert response.json()[0]["date"] == "string"
+
+def test_put_todo_update(db):
+    response = client.put(
+        "/6233fa4fdafac261d4ceacf0",
+        json={
+            "name": "update",
+            "description": "string2",
+            "completed": "true",
+            "date": "string"
+        }
+    )
+    assert response.status_code == 200
+    assert response.json()[0]["name"] == "update"
+    assert response.json()[0]["description"] == "string2"
+    assert response.json()[0]["completed"] == True
+    assert response.json()[0]["date"] == "string"
+
+def test_todo_delete(db):
+    response = client.delete(
+        "/6233fa4fdafac261d4ceacf0"
+    )
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
